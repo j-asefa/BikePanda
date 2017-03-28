8dab9606e265809da5d0211edd7b50476b4ed492
James Asefa <jamesasefa@gmail.com>
2017-03-28 13:17:35 -0700

    live and static graphs now working

--- Website/speedvstime.php
+++ Website/speedvstime.php
@@ -1,7 +1,8 @@
 <?php
 
+// this only serves GET requests
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'GET') != 0){
-        die('Get request failed');
+        die('Get requests only');
 }
 $conn = new mysqli("localhost", "jamie", "3parj9Ld5Rs18", "test_db");
 
@@ -11,33 +12,55 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
+if(!isset($_GET['tripid'])) {
+	die('Get request failed on parameter');
+}
+
 $speed = 0;
 $time = "";
-$speedarr = array();
-$timearr = array();
 $arr = array();
-$trip_number = intval($_GET["tripid"]);
+$trip_number = $_GET['tripid'];
+$trip_start = 0;
 
-if($stmt = $conn->prepare("SELECT speed, time FROM bikedata WHERE trip_number = ?")) {
-        $stmt->bind_param("i",$trip_number);
+
+// get the time stamp for the beginning of the trip
+if($stmt1 = $conn->prepare("SELECT MIN(time) as time FROM bikedata WHERE trip_number = ?")) {
+        $stmt1->bind_param("i",$trip_number);
 } else {
         die("Prepared statement failed failed: " . $conn->error_list);
 }
 
-if (!$stmt->execute()) {
-    die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+if (!$stmt1->execute()) {
+    die("Execute failed: (" . $stmt1->errno . ") " . $stmt1->error);
 }
 
-$stmt->bind_result($speed, $time);
+$stmt1->bind_result($trip_start);
+$stmt1->fetch();
+$stmt1->close();
 
-while($stmt->fetch()) {
-	array_push($arr, ['speed' => $speed, 'time' => $time]);
-	//$arr += ['speed' => $speed, 'time' => $time];
-	/*$speedarr = ['speed' => $speed];	
-	$timearr = ['time' => $time];
-	$arr += [$speedarr, $timearr];
-	echo json_encode($arr);*/
+
+// now get the rest of the data for the trip
+if($stmt2 = $conn->prepare("SELECT speed, time FROM bikedata WHERE trip_number = ?")) {
+        $stmt2->bind_param("i",$trip_number);
+} else {
+        die("Prepared statement failed failed: " . $conn->error_list);
 }
+
+if (!$stmt2->execute()) {
+    die("Execute failed: (" . $stmt2->errno . ") " . $stmt2->error);
+}
+
+$stmt2->bind_result($speed, $time);
+
+// fetch the row values of speed and time
+while($stmt2->fetch()) {
+	$time_from_start = strtotime($time) - strtotime($trip_start); // seconds from start of trip
+	array_push($arr, ['speed' => $speed, 'time' => $time_from_start]);
+}
+
+// add the date of the trip and send results
+array_push($arr, ["start_time" => $trip_start]);
 echo json_encode($arr);
+$stmt2->close();
 $conn->close();
 ?>
